bba38c67bb6004ec848289821755e85e37aaded4
Mandrillus <32806400+Mandrillus@users.noreply.github.com>
2017-10-22 22:38:00 +0900

    [Add] アプリ完成
    アプリ作成終了.

--- index.js
+++ index.js
@@ -3,7 +3,30 @@
 var electron = require('electron');
 var remote = electron.remote;
 var fileUtil = remote.require('./lib/fileUtil');
+var matched = location.search.match(/baseDir=([^&]*)/);
+var baseDir = matched && decodeURIComponent(matched[1]);
+var ngModule = angular.module('readUs', []);
 
-fileUtil.fetchReadmeList(function(err, matches) {
-  if(!err) document.write(matches.join());
+ngModule.controller('MainController', function ($scope) {
+  var main = this;
+
+  // README.mdの取得
+  main.getFile = function(file) {
+    main.fileText = fileUtil.getAsText(file.filepath);
+  };
+
+  fileUtil.fetchReadmeList(baseDir, function (err, fileList) {
+    if(err) console.error(err);
+    $scope.$apply(function () {
+      main.fileList = fileList;
+    });
+  });
+});
+
+ngModule.directive('mdPreview', function () {
+  return function ($scope, $elem, $attrs) {
+    $scope.$watch($attrs.mdPreview, function(source) {
+      $elem.html(marked(source));
+    });
+  };
 });
--- main.js
+++ main.js
@@ -3,21 +3,64 @@
 var electron = require('electron');
 var app = electron.app;
 var BrowserWindow = electron.BrowserWindow;
+var Menu = electron.Menu;
+const {dialog} = require('electron')
 
-var mainWindow = null;
+
+const {crashReporter} = require('electron');
+
+crashReporter.start({
+  productName: 'YourName',
+  companyName: 'YourCompany',
+  submitURL: 'https://your-domain.com/url-to-submit',
+  autoSubmit: true
+});
 
 app.on('window-all-closed', function() {
-  if (process.platform != 'darwin')
     app.quit();
 });
 
 app.on('ready', function() {
+  // メニューをアプリケーションに追加
+  Menu.setApplicationMenu(menu);
 
-  // ブラウザ(Chromium)の起動, 初期画面のロード
-  mainWindow = new BrowserWindow({width: 800, height: 600});
-  mainWindow.loadURL('file://' + __dirname + '/index.html');
-
-  mainWindow.on('closed', function() {
-    mainWindow = null;
-  });
+  openWindow(process.cwd());
 });
+
+function openWindow (baseDir) {
+  var win = new BrowserWindow({width: 800, height: 600});
+  win.loadURL('file://' + __dirname + '/index.html?baseDir=' + encodeURIComponent(baseDir));
+  win.on('closed', function () {
+    win = null;
+  });
+}
+
+// メニュー情報の作成
+var template = [
+  {
+    label: 'ReadUs',
+    submenu: [
+      {label: 'Quit', accelerator: 'Command+Q', click: function () {app.quit();}}
+    ]
+  }, {
+    label: 'File',
+    submenu: [
+      {label: 'Open', accelerator: 'Command+O', click: function() {
+        // 「ファイルを開く」ダイアログの呼び出し
+        dialog.showOpenDialog({ properties: ['openDirectory']}, function (baseDir){
+          if(baseDir && baseDir[0]) {
+            openWindow(baseDir[0]);
+          }
+        });
+      }}
+    ]
+  }, {
+    label: 'View',
+    submenu: [
+      { label: 'Reload', accelerator: 'Command+R', click: function() { BrowserWindow.getFocusedWindow().reload(); } },
+      { label: 'Toggle DevTools', accelerator: 'Alt+Command+I', click: function() { BrowserWindow.getFocusedWindow().toggleDevTools(); } }
+    ]
+  }
+];
+
+var menu = Menu.buildFromTemplate(template);
